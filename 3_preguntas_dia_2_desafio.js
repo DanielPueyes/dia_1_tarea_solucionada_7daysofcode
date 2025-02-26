ef645175ce5cbfb11a7e786604aeea7eac0e8ea1
DanielPueyes <danielpueyes1988@gmail.com>
2025-02-26 20:08:25 -0300

    Update 3_preguntas_dia_2_desafio.js

--- 3_preguntas_dia_2_desafio.js
+++ 3_preguntas_dia_2_desafio.js
@@ -1,1 +1,13 @@
+let nombre = prompt("¿Cuál es tu nombre?");
+let edad = prompt("¿Cuántos años tienes?");
+let lenguajeProgramacion = prompt("¿Que lenguaje de programación estás estudiando?");
 
+alert(`Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ¡${lenguajeProgramacion}¡`);
+
+let respuesta = parseInt(prompt(`¿ Te gusta estudiar ${lenguajeProgramacion}?, Responde con el número 1 para SÍ o 2 para NO.`));
+if(respuesta == 1){
+    alert("¡Muy bien! Sigue estudiando y tendras mucho éxito.");
+}if(respuesta == 2){
+    alert("Oh, qué pena...¿Ya intentaste aprender otros lenguajes?");
+
+}
